5295e4fa8dcd80d1d3af5d3c96150ce65be008c0
Cable <c4813eve@gmail.com>
2025-08-03 14:35:07 +0100

    Update eve-reprocess-trading.php

--- eve-reprocess-trading/eve-reprocess-trading.php
+++ eve-reprocess-trading/eve-reprocess-trading.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: EVE Reprocess Trading
 Description: Displays trade hub mineral prices, brokerage and tax estimates based on skills and standings.
-Version: 0.2.7
+Version: 0.3
 Author: C4813
 */
 
